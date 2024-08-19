35bae3b742710e445eb9439593d4851659c5c81a
hr-nmm <himanshu_code@hotmail.com>
2024-08-19 23:41:47 +0530

    init commit add basic crud operations on dummy data

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+// fetch all persons
+app.get("/api/persons", (_, res) => res.json(persons));
+
+// fetch info
+app.get("/api/info", (_, res) => {
+  res.send(
+    `<p>
+      Phonebook has info for ${persons.length} people <br />
+      ${new Date()}
+    </p>`
+  );
+});
+
+// get single resource
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+  person ? res.json(person) : res.sendStatus(404).end("<p>enter valid url</p>");
+});
+
+// delete resource
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  res.sendStatus(240).end();
+});
+
+// create resource
+const generateID = () => Math.floor(Math.random() * 10000000 + 1).toString();
+app.post("/api/persons/", (req, res) => {
+  const body = req.body;
+  if (
+    !body.name ||
+    !body.number ||
+    persons.find((person) => person.name === body.name)
+  ) {
+    return res.status(400).json({
+      error: "content missing/duplicate",
+    });
+  }
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`ran successfully`);
+});
